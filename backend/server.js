3d64b87ceb771724e155556eb014c44e6b873d88
ankkitsirg <ankkitsirg@gmail.com>
2026-03-26 14:00:11 +0530

    first version of project

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');//constructor
+const cors = require("cors");//constructor
+
+const obj = express();//calling constructor
+
+//use the permission 
+obj.use(cors());
+obj.use(express.json());
+
+//obj.get('interfacename',working)
+obj.get("/getdata", (objreq,objres) => {
+         objres.send("API is running");
+});
+
+obj.listen(5000,()=>{
+    console.log("server is listening at 5000 port");
+})
